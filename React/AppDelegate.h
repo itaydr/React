ab0cb15ee36b6ae9e19c4eef9240234e0b5ea675
Itay Dressler <itaydressler@gmail.com>
2015-03-22 22:08:21 +0200

    Initial Commit

---
+++ React/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  React
+//
+//  Created by Itay Dressler on 3/22/15.
+//  Copyright (c) 2015 react. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ React/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  React
+//
+//  Created by Itay Dressler on 3/22/15.
+//  Copyright (c) 2015 react. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

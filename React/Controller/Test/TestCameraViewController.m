1ee2c07067d4061eefb20f82816e4865b2d3b0dd
Itay Dressler <itaydressler@gmail.com>
2015-03-25 03:21:42 +0200

    Capturing and playing multiple videos, saving to asset lib.

--- React/Controller/Test/TestCameraViewController.m
+++ React/Controller/Test/TestCameraViewController.m
@@ -9,6 +9,7 @@
 #import "TestCameraViewController.h"
 #import <PBJVision.h>
 #import <MediaPlayer/MediaPlayer.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface TestCameraViewController () <PBJVisionDelegate>
 
@@ -16,19 +17,29 @@
 @property (nonatomic, strong)   AVCaptureVideoPreviewLayer *previewLayer;
 @property (nonatomic, strong)   UILongPressGestureRecognizer *longPress;
 @property (nonatomic, assign)   BOOL    isRecording;
+@property (nonatomic, strong)   NSMutableArray *moviePlayers;
 
 @end
 
-@implementation TestCameraViewController
+@implementation TestCameraViewController {
+    ALAssetsLibrary *_assetLibrary;
+}
+
+- (void)dealloc {
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    _assetLibrary = [[ALAssetsLibrary alloc] init];
     // Do any additional setup after loading the view.
     [self setupCameraView];
     [self _setup];
     [[PBJVision sharedInstance] setCameraDevice:PBJCameraDeviceFront];
     self.longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPressGestureRecognizer:)];
     [self.previewView addGestureRecognizer:_longPress];
+    self.moviePlayers = [NSMutableArray array];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -96,6 +107,104 @@
 - (void)vision:(PBJVision *)vision capturedVideo:(NSDictionary *)videoDict error:(NSError *)error {
     NSLog(@"Finished video capture");
     self.isRecording = NO;
+    
+    [self performSelectorInBackground:@selector(saveVideoAndPlay:) withObject:videoDict];
+}
+
+- (void)saveVideoAndPlay:(NSDictionary*)videoDict {
+    NSString *videoPath = [videoDict  objectForKey:PBJVisionVideoPathKey];
+    [_assetLibrary writeVideoAtPathToSavedPhotosAlbum:[NSURL URLWithString:videoPath] completionBlock:^(NSURL *assetURL, NSError *error1) {
+        [self performSelectorOnMainThread:@selector(playMultipleVideos:) withObject:videoDict waitUntilDone:NO];
+    }];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    
+}
+
+static int i = 0;
+
+- (void)playMultipleVideos:(NSDictionary*)videoDict {
+    
+    NSURL *video = [NSURL fileURLWithPath:[videoDict objectForKey:PBJVisionVideoPathKey]];
+    // Create an AVURLAsset with an NSURL containing the path to the video
+    AVURLAsset *asset = [AVURLAsset URLAssetWithURL:video options:nil];
+    
+    // Create an AVPlayerItem using the asset
+    AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:asset];
+    
+    // Create the AVPlayer using the playeritem
+    AVPlayer *player = [AVPlayer playerWithPlayerItem:playerItem];
+    
+    // Create an AVPlayerLayer using the player
+    AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer:player];
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(i++ * 60, self.view.bounds.size.height - 100, 50, 50)];
+    // Add it to your view's sublayers
+    playerLayer.frame = view.bounds;
+    [view.layer addSublayer:playerLayer];
+    view.layer.cornerRadius = 25;
+    view.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:view];
+    
+    if (i ==1){
+        [player play];
+    }
+    //player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
+    // You can play/pause using the AVPlayer object
+    
+    // It is also useful to use the AVPlayerItem's notifications and Key-Value
+    // Observing on the AVPlayer's status and the AVPlayerLayer's readForDisplay property
+    // (to know when the video is ready to be played, if for example you want to cover the
+    // black rectangle with an image until the video is ready to be played)
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playerItemDidReachEnd:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:[player currentItem]];
+    
+//    [player addObserver:self forKeyPath:@"currentItem.status"
+//                options:0
+//                context:nil];
+//    
+//    [playerLayer addObserver:self forKeyPath:@"readyForDisplay"
+//                     options:0
+//                     context:nil];
+    
+    [self.moviePlayers addObject:player];
+}
+
+- (void)playerItemDidReachEnd:(NSNotification*)notification {
+    AVPlayerItem *p = [notification object];
+    [p seekToTime:kCMTimeZero];
+    
+    for (AVPlayer *player in self.moviePlayers) {
+        if ([player.currentItem isEqual:p]) {
+            [player play];
+        }
+        else if (player.status == AVPlayerStatusReadyToPlay) {
+            [player play];
+        }
+    }
+}
+
+- (void)playMultipleVideosInRepeat:(NSDictionary*)videoDict {
+    
+    NSURL *video = [NSURL fileURLWithPath:[videoDict objectForKey:PBJVisionVideoPathKey]];
+    
+    MPMoviePlayerController *controller = [[MPMoviePlayerController alloc] initWithContentURL:video];
+    [controller prepareToPlay];
+    controller.repeatMode = MPMovieRepeatModeOne;
+    controller.controlStyle = MPMovieControlStyleNone;
+    controller.view.frame = CGRectMake(i++ * 60, self.view.bounds.size.height - 100, 50, 50);
+    [self.view addSubview:controller.view];
+    
+    [self.moviePlayers addObject:controller];
+    controller.view.layer.cornerRadius = 0.5 * 50;
+    
+    [controller play];
+}
+
+- (void)playFullView:(NSDictionary*)videoDict {
     MPMoviePlayerViewController *movieController = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL fileURLWithPath:[videoDict objectForKey:PBJVisionVideoPathKey]]];
     [self presentMoviePlayerViewControllerAnimated:movieController];
     movieController.moviePlayer.view.layer.cornerRadius = 0.5 * movieController.moviePlayer.view.bounds.size.width;
